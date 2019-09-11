deafe3b015e2b8229c4f0731d2606ec9b898425e
LMC <2_minchul@naver.com>
2019-09-12 03:12:45 +0900

    p77 문제 풀이

--- p77/python/test_p77.py
+++ p77/python/test_p77.py
@@ -1,6 +1,14 @@
-from .p77 import merge
-
 def test_p77_merge():
+    from .p77 import merge
+    _test_merge(merge)
+
+
+def test_my_solution_merge():
+    from .my_solution import merge
+    _test_merge(merge)
+
+
+def _test_merge(merge):
     intervals = [(1, 3), (5, 8), (4, 10), (20, 25)]
     assert merge(intervals) == [(1, 3), (4, 10), (20, 25)]
 
